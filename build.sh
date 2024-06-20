03ec36a5e211e40f3bd0cf37a3a680e3177d8471
sbstnrmrz <ramicastasebas@gmail.com>
2024-06-20 09:30:32 -0400

    updated build scripts

--- build.sh
+++ build.sh
@@ -4,18 +4,17 @@ CC="clang++"
 CCFLAGS="-Wall -Wextra"
 IDIR="/usr/local/include"
 LDIR="/usr/local/lib"
-LIBS="-lm -lSDL3 -lSDL3_image -lSDL3_ttf"
+LIBS="-lm"
 SRC="*.cpp"
 RPATH=""
-BAT="set SRC=$SRC
-set CC=g++
-set CFLAGS=$CCFLAGS
-set IDIR=C:/msys64/ucrt64/include
-set LDIR=C:/msys64/ucrt64/lib
-set SRC=$SRC
-set LIBS=$LIBS"
-
-echo $BAT >> build_win.bat 
+BAT="set SRC=$SRC\n
+set CC=g++\n
+set CFLAGS=$CCFLAGS\n
+set IDIR=C:/msys64/ucrt64/include\n
+set LDIR=C:/msys64/ucrt64/lib\n
+set SRC=$SRC\n
+set LIBS=$LIBS\n
+%CC% %CFLAGS% %SRC% -o main -I%IDIR% -L%LDIR% %LIBS%"
 
 if [ `uname` = "Darwin" ]; then
     RPATH+="-rpath $LDIR"
@@ -29,5 +28,7 @@ if ! test -f build_win.bat; then
     echo "build_win.bat created!"
     touch build_win.bat 
 fi
+
+echo -e $BAT > build_win.bat 
 
 $CC $CCFLAGS $SRC -o main -I$IDIR -L$LDIR $LIBS $RPATH 
